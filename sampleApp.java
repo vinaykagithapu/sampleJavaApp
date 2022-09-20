c72a49bad8f314218ddc34c636d0abee15de7a85
vinaykagithapu <33367418+vinaykagithapu@users.noreply.github.com>
2022-09-20 15:40:40 +0530

    Initial commit

---
+++ sampleApp.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Sample Java\n");
+    System.out.println("Welcome to you sample App\n");
+    System.out.println("Thank you\n");
+  }
+}
