04d71c6704575458d89907fceb1d08e710ffd02e
vinaykagithapu <33367418+vinaykagithapu@users.noreply.github.com>
2022-09-20 16:19:21 +0530

    Update sampleApp.java

--- sampleApp.java
+++ sampleApp.java
@@ -1,7 +1,7 @@
 public class sampleApp {
   public static void main(String[] args) {
-    System.out.println("Sample Java\n");
-    System.out.println("Welcome to you sample App\n");
+    System.out.println("\n\nSample Java\n");
+    System.out.println("Welcome to sample App\n");
     System.out.println("Thank you\n");
   }
 }
